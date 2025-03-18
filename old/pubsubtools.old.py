8c1dfce7a37c09a799c29e3363e4622776236f60
chunk <chunk@zez.local.packets.cc>
2025-03-18 08:27:56 -0600

    moving things around, making more sense

---
+++ old/pubsubtools.old.py
@@ -0,0 +1,147 @@
+
+
+# from getpass import getpass
+# from pubsub_commands import PubSubCommands as ps_commands
+# import asyncio
+# import slixmpp
+# import logging
+# import os
+# import random
+
+# class PubSubTools(slixmpp.ClientXMPP):
+
+# 	def __init__(self, jid, server, node=None, data=''):
+#         super().__init__(jid, password)
+
+# 		self.jid = jid
+# 		self.password = password
+# 		self.room = room
+# 		self.nick = nick
+# 		self.pubsub_server = server
+# 		self.add_event_handler("session_start", self.start)
+# 		self.add_event_handler("groupchat_message", self.muc_message)
+
+# 	async def start(self, event):
+# 		await self.get_roster()
+# 		self.send_presence()
+# 		self.plugin['xep_0045'].join_muc(self.room, self.nick)
+
+# 	def muc_message(self, msg):
+# 	# ### COMMANDS:
+# 		body = msg['body']
+# 		parts = body.split(' ')
+
+# 		# GET NODES:
+# 		async def nodes(self):
+# 			nodes = await ps_commands.nodes(self)
+# 			msg.reply(nodes).send()
+
+# 		if body == "!xmpp nodes":
+# 			nodes(self)
+
+# 		# if len(parts) > 2:
+# 		# 	if parts[0] == "!xmpp":
+# 		# 		if len(parts) == 3: # 1 args: node
+# 		# 			node = parts[2]
+# 		# 			node = str(node)
+					
+# 		# 			if parts[1] == "get":
+# 		# 				# variable = method():
+# 		# 				get = ps_commands.get(self, node)
+# 				# 	elif parts[1] == "create":
+# 				# 		# variable = method():
+# 				# 		create = ps_commands.create(self, node)
+# 				# 	elif parts[1] == "delete":
+# 				# 		# variable = method():
+# 				# 		delete = ps_commands.delete(self, node)
+# 				# 	elif parts[1] == "purge":
+# 				# 		# variable = method():
+# 				# 		purge = ps_commands.purge(self, node)
+# 				# 	elif parts[1] == "subscribe":
+# 				# 		# variable = method():
+# 				# 		subscribe = ps_commands.subscribe(self, node)
+# 				# 	elif parts[1] == "unsubscribe":
+# 				# 		# variable = method():
+# 				# 		unsubscribe = ps_commands.unsubscribe(self, node)
+# 				# 	else:
+# 				# 		pass # write to muc (maybe) error of ____
+
+# 				# # publish, retract:
+# 				# elif len(parts) == 4: # 2 args: node, data
+# 				# 	pass
+
+
+
+# 	# SERVICE DISCOVERY:
+# 	## NOT PUBSUB!
+# 		# GET LOCAL INFO:
+# 		async def get_info(self):
+# 			info = await self['xep_0030'].get_info(node=self.nick, local=True)
+# 			# info_list = info.split('\n')
+# 			# info_str = "\r\n".join(info_list)
+# 			msg.reply("I have yet to be figured out!").send() # !!!!!!!!!!!!!!!!!!!!
+
+# 		if body == "!xmpp getinfo":
+# 			get_info(self)
+
+# 		# GET LOCAL NODE ITEMS:
+# 		async def items(self, node):
+# 					try:
+# 						items = await self['xep_0030'].get_items(jid=self.jid, node=node, local=True)
+# 						items_string = "\r\n".join(items)
+# 						msg.reply(items_string).send()
+# 					except:
+# 						mesg = "Invalid Node!"
+# 						msg.reply(mesg).send()
+
+# 		if body.startswith('!xmpp items '):
+# 			if len(parts) > 2:
+# 				node = parts[2]
+# 				node = str(node)
+# 				items(self, node)
+
+# 	# INFORMATIONAL:
+# 		# ABOUT:
+# 		if msg['body'] == "!xmpp about":
+# 			mesgs = [
+# 				"xmpp:xmpptools@packets.cc",
+# 				"xmpp:pubsubtools@packets.cc",
+# 				"PubSubTools - A SliXMPP Bot Project - by chunk",
+# 				"https://github.com/the8woodcutter/pubsubtools"
+# 			]
+# 			mesg = "\r\n".join(mesgs)
+# 			msg.reply(mesg).send()
+
+# 		# HELP:
+# 		if msg['body'] == "!xmpp help":
+# 			mesgs = [
+# 				"`!xmpp nodes` to get nodes from pubsub server",
+# 				"`!xmpp get <node_name>` to get node items from node",
+# 			]
+# 			mesg = "\r\n".join(mesgs)
+# 			msg.reply(mesg).send()
+
+
+
+# if __name__ == '__main__':
+# 	botjid_to_load_in = input("Bot's JID: ")
+# 	botjid_to_load = str(botjid_to_load_in)
+
+# 	passwd_to_load_in =  input("Password: ")
+# 	passwd_to_load = str(passwd_to_load_in)
+
+# 	botnick_to_load_in =  input("Bot's nickname: ")
+# 	botnick_to_load = str(botnick_to_load_in)
+
+# 	muc_to_load_in =  input("MUC's JID: ")
+# 	muc_to_load = str(muc_to_load_in)
+
+# 	xmpp = PubSubTools(botjid_to_load, passwd_to_load, muc_to_load, botnick_to_load)
+# 	xmpp.register_plugin('xep_0030')  # Service Discovery
+# 	xmpp.register_plugin('xep_0045')  # Multi-User Chat
+# 	xmpp.register_plugin('xep_0059')  # Result Set Management
+# 	xmpp.register_plugin('xep_0060')  # PubSub
+# 	xmpp.register_plugin('xep_0199')  # XMPP Ping
+# 	xmpp.connect()
+# 	# xmpp.process()
+# 	asyncio.get_event_loop().run_forever()
--- pubsubtools.py
+++ pubsubtools.py
@@ -1,145 +1,179 @@
-from getpass import getpass
-from pubsub_commands import PubSubCommands as ps_commands
-import asyncio
-import slixmpp
+#!/usr/bin/env python3
+
 import logging
-import os
-import random
+from getpass import getpass
+from argparse import ArgumentParser
 
-class PubSubTools(slixmpp.ClientXMPP):
-
-	def __init__(self, jid, password, room, nick):
-		slixmpp.ClientXMPP.__init__(self, jid, password)
-
-		self.jid = jid
-		self.password = password
-		self.room = room
-		self.nick = nick
-		self.pubsub_server = "pubsub.xmpp.packets.cc"
-		self.add_event_handler("session_start", self.start)
-		self.add_event_handler("groupchat_message", self.muc_message)
-
-	async def start(self, event):
-		await self.get_roster()
-		self.send_presence()
-		self.plugin['xep_0045'].join_muc(self.room, self.nick)
-
-	def muc_message(self, msg):
-	# ### COMMANDS:
-		body = msg['body']
-		parts = body.split(' ')
-
-		# GET NODES:
-		async def nodes(self):
-			nodes = await ps_commands.nodes(self)
-			msg.reply(nodes).send()
-
-		if body == "!xmpp nodes":
-			nodes(self)
-
-		# if len(parts) > 2:
-		# 	if parts[0] == "!xmpp":
-		# 		if len(parts) == 3: # 1 args: node
-		# 			node = parts[2]
-		# 			node = str(node)
-					
-		# 			if parts[1] == "get":
-		# 				# variable = method():
-		# 				get = ps_commands.get(self, node)
-				# 	elif parts[1] == "create":
-				# 		# variable = method():
-				# 		create = ps_commands.create(self, node)
-				# 	elif parts[1] == "delete":
-				# 		# variable = method():
-				# 		delete = ps_commands.delete(self, node)
-				# 	elif parts[1] == "purge":
-				# 		# variable = method():
-				# 		purge = ps_commands.purge(self, node)
-				# 	elif parts[1] == "subscribe":
-				# 		# variable = method():
-				# 		subscribe = ps_commands.subscribe(self, node)
-				# 	elif parts[1] == "unsubscribe":
-				# 		# variable = method():
-				# 		unsubscribe = ps_commands.unsubscribe(self, node)
-				# 	else:
-				# 		pass # write to muc (maybe) error of ____
-
-				# # publish, retract:
-				# elif len(parts) == 4: # 2 args: node, data
-				# 	pass
+import slixmpp
+from slixmpp.exceptions import XMPPError
+from slixmpp.xmlstream import ET, tostring
 
 
+class PubsubClient(slixmpp.ClientXMPP):
 
-	# SERVICE DISCOVERY:
-	## NOT PUBSUB!
-		# GET LOCAL INFO:
-		async def get_info(self):
-			info = await self['xep_0030'].get_info(node=self.nick, local=True)
-			# info_list = info.split('\n')
-			# info_str = "\r\n".join(info_list)
-			msg.reply("I have yet to be figured out!").send() # !!!!!!!!!!!!!!!!!!!!
+    def __init__(self, jid, password, room, nick, server,
+                       node=None, action='nodes', data=''):
+        super().__init__(jid, password)
 
-		if body == "!xmpp getinfo":
-			get_info(self)
+        self.register_plugin('xep_0030')
+        self.register_plugin('xep_0059')
+        self.register_plugin('xep_0060')
+        self.register_plugin('xep_0045')
 
-		# GET LOCAL NODE ITEMS:
-		async def items(self, node):
-					try:
-						items = await self['xep_0030'].get_items(jid=self.jid, node=node, local=True)
-						items_string = "\r\n".join(items)
-						msg.reply(items_string).send()
-					except:
-						mesg = "Invalid Node!"
-						msg.reply(mesg).send()
+        # self.actions = ['nodes', 'create', 'delete', 'get_configure',
+        #                 'publish', 'get', 'retract',
+        #                 'purge', 'subscribe', 'unsubscribe']
 
-		if body.startswith('!xmpp items '):
-			if len(parts) > 2:
-				node = parts[2]
-				node = str(node)
-				items(self, node)
+        # self.action = action
+        self.room = room
+        self.nick = nick
+        self.node = node
+        self.data = data
+        self.pubsub_server = server
 
-	# INFORMATIONAL:
-		# ABOUT:
-		if msg['body'] == "!xmpp about":
-			mesgs = [
-				"xmpp:xmpptools@packets.cc",
-				"xmpp:pubsubtools@packets.cc",
-				"PubSubTools - A SliXMPP Bot Project - by chunk",
-				"https://github.com/the8woodcutter/pubsubtools"
-			]
-			mesg = "\r\n".join(mesgs)
-			msg.reply(mesg).send()
+        self.add_event_handler('session_start', self.start)
+        self.add_event_handler('groupchat_message', self.muc_message)
 
-		# HELP:
-		if msg['body'] == "!xmpp help":
-			mesgs = [
-				"`!xmpp nodes` to get nodes from pubsub server",
-				"`!xmpp get <node_name>` to get node items from node",
-			]
-			mesg = "\r\n".join(mesgs)
-			msg.reply(mesg).send()
+    async def start(self, event):
+        await self.get_roster()
+        self.send_presence()
+        self.plugin['xep_0045'].join_muc(self.room, self.nick)
+
+        # try:
+        #     await getattr(self, self.action)()
+        # except:
+        #     logging.exception('Could not execute %s:', self.action)
+        # self.disconnect()
+
+    def muc_message(self, msg):
+        # do the stuff where messages make commands:              
+
+# make these commands return message strings:
+    async def nodes(self):
+        try:
+            result = await self['xep_0060'].get_nodes(self.pubsub_server, self.node)
+            for item in result['disco_items']['items']:
+                logging.info('  - %s', str(item))
+        except XMPPError as error:
+            logging.error('Could not retrieve node list: %s', error.format())
+
+    async def create(self):
+        try:
+            await self['xep_0060'].create_node(self.pubsub_server, self.node)
+            logging.info('Created node %s', self.node)
+        except XMPPError as error:
+            logging.error('Could not create node %s: %s', self.node, error.format())
+
+    async def delete(self):
+        try:
+            await self['xep_0060'].delete_node(self.pubsub_server, self.node)
+            logging.info('Deleted node %s', self.node)
+        except XMPPError as error:
+            logging.error('Could not delete node %s: %s', self.node, error.format())
+
+    async def get_configure(self):
+        try:
+            configuration_form = await self['xep_0060'].get_node_config(self.pubsub_server, self.node)
+            logging.info('Configure form received from node %s: %s', self.node, configuration_form['pubsub_owner']['configure']['form'])
+        except XMPPError as error:
+            logging.error('Could not retrieve configure form from node %s: %s', self.node, error.format())
+
+    async def publish(self):
+        payload = ET.fromstring("<test xmlns='test'>%s</test>" % self.data)
+        try:
+            result = await self['xep_0060'].publish(self.pubsub_server, self.node, payload=payload)
+            logging.info('Published at item id: %s', result['pubsub']['publish']['item']['id'])
+        except XMPPError as error:
+            logging.error('Could not publish to %s: %s', self.node, error.format())
+
+    async def get(self):
+        try:
+            result = await self['xep_0060'].get_item(self.pubsub_server, self.node, self.data)
+            for item in result['pubsub']['items']['substanzas']:
+                logging.info('Retrieved item %s: %s', item['id'], tostring(item['payload']))
+        except XMPPError as error:
+            logging.error('Could not retrieve item %s from node %s: %s', self.data, self.node, error.format())
+
+    async def retract(self):
+        try:
+            await self['xep_0060'].retract(self.pubsub_server, self.node, self.data)
+            logging.info('Retracted item %s from node %s', self.data, self.node)
+        except XMPPError as error:
+            logging.error('Could not retract item %s from node %s: %s', self.data, self.node, error.format())
+
+    async def purge(self):
+        try:
+            await self['xep_0060'].purge(self.pubsub_server, self.node)
+            logging.info('Purged all items from node %s', self.node)
+        except XMPPError as error:
+            logging.error('Could not purge items from node %s: %s', self.node, error.format())
+
+    async def subscribe(self):
+        try:
+            iq = await self['xep_0060'].subscribe(self.pubsub_server, self.node)
+            subscription = iq['pubsub']['subscription']
+            logging.info('Subscribed %s to node %s', subscription['jid'], subscription['node'])
+        except XMPPError as error:
+            logging.error('Could not subscribe %s to node %s: %s', self.boundjid.bare, self.node, error.format())
+
+    async def unsubscribe(self):
+        try:
+            await self['xep_0060'].unsubscribe(self.pubsub_server, self.node)
+            logging.info('Unsubscribed %s from node %s', self.boundjid.bare, self.node)
+        except XMPPError as error:
+            logging.error('Could not unsubscribe %s from node %s: %s', self.boundjid.bare, self.node, error.format())
 
 
 
-if __name__ == '__main__':
-	botjid_to_load_in = input("Bot's JID: ")
-	botjid_to_load = str(botjid_to_load_in)
 
-	passwd_to_load_in =  input("Password: ")
-	passwd_to_load = str(passwd_to_load_in)
+# if __name__ == '__main__':
+#     # Setup the command line arguments.
+#     parser = ArgumentParser()
+#     parser.version = '%%prog 0.1'
+#     parser.usage = "Usage: %%prog [options] <jid> " + \
+#                              'nodes|create|delete|get_configure|purge|subscribe|unsubscribe|publish|retract|get' + \
+#                              ' [<node> <data>]'
 
-	botnick_to_load_in =  input("Bot's nickname: ")
-	botnick_to_load = str(botnick_to_load_in)
+#     parser.add_argument("-q","--quiet", help="set logging to ERROR",
+#                         action="store_const",
+#                         dest="loglevel",
+#                         const=logging.ERROR,
+#                         default=logging.INFO)
+#     parser.add_argument("-d","--debug", help="set logging to DEBUG",
+#                         action="store_const",
+#                         dest="loglevel",
+#                         const=logging.DEBUG,
+#                         default=logging.INFO)
 
-	muc_to_load_in =  input("MUC's JID: ")
-	muc_to_load = str(muc_to_load_in)
+#     # JID and password options.
+#     parser.add_argument("-j", "--jid", dest="jid",
+#                         help="JID to use")
+#     parser.add_argument("-p", "--password", dest="password",
+#                         help="password to use")
 
-	xmpp = PubSubTools(botjid_to_load, passwd_to_load, muc_to_load, botnick_to_load)
-	xmpp.register_plugin('xep_0030')  # Service Discovery
-	xmpp.register_plugin('xep_0045')  # Multi-User Chat
-	xmpp.register_plugin('xep_0059')  # Result Set Management
-	xmpp.register_plugin('xep_0060')  # PubSub
-	xmpp.register_plugin('xep_0199')  # XMPP Ping
-	xmpp.connect()
-	# xmpp.process()
-	asyncio.get_event_loop().run_forever()
+#     parser.add_argument("server")
+#     parser.add_argument("action", choices=["nodes", "create", "delete", "get_configure", "purge", "subscribe", "unsubscribe", "publish", "retract", "get"])
+#     parser.add_argument("node", nargs='?')
+#     parser.add_argument("data", nargs='?')
+
+#     args = parser.parse_args()
+
+#     # Setup logging.
+#     logging.basicConfig(level=args.loglevel,
+#                         format='%(levelname)-8s %(message)s')
+
+#     if args.jid is None:
+#         args.jid = input("Username: ")
+#     if args.password is None:
+#         args.password = getpass("Password: ")
+
+#     # Setup the Pubsub client
+#     xmpp = PubsubClient(args.jid, args.password,
+#                         server=args.server,
+#                         node=args.node,
+#                         action=args.action,
+#                         data=args.data)
+
+#     # Connect to the XMPP server and start processing XMPP stanzas.
+#     xmpp.connect()
+#     xmpp.process(forever=False)
